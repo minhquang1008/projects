abde609f3f4ec678bde338f31db0fb8ccc7f5124
minhquang1008 <115976900+minhquang1008@users.noreply.github.com>
2024-01-08 13:04:55 +0700

    Add files via upload

--- Streamlit/Data_Dictionary/searcher.py
+++ Streamlit/Data_Dictionary/searcher.py
@@ -3,26 +3,30 @@ import pandas as pd
 
 
 # sheet_list = pd.ExcelFile(f'https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=xlsx').sheet_names
-def get_updated(table, information_table, margin_table, trading_table):
-    sheet_id = '1Rgst7ZPw2b9dj4YXwa24vHzLaLEq4vuv'
+def get_updated(table, company_table, margin_table, trading_table, customer_table):
+    sheet_id = '1f97mxqZXFH1I9kE6Y4f0vB5oMf1lhBtmDX3lOvEXzeo'
     column_list = []
     for i in table:
+        print(i)
         df = pd.read_excel(
             f'https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=xlsx',
             sheet_name=i,
-            header=2
+            header=1
         )
         with open(f'{i}.pkl', 'wb') as file:
             pickle.dump(df, file)
-        lst = df['DataMart Column'].dropna().unique().tolist()
-        if i in information_table:
-            lst = [f'Information/{i}/' + k for k in lst]
+        lst = df['COLUMN.3'].dropna().unique().tolist()
+        if i in company_table:
+            lst = [f'Company Information/{i}/' + k for k in lst]
         elif i in margin_table:
             lst = [f'Margin/{i}/' + k for k in lst]
         elif i in trading_table:
             lst = [f'Trading/{i}/' + k for k in lst]
+        elif i in customer_table:
+            lst = [f'Customer Information/{i}/' + k for k in lst]
         [column_list.append(k) for k in lst]
     for i in column_list:
+        print(i)
         if column_list.index(i) == 0:
             column_list.insert(0, i.split('/')[0])
             column_list.insert(1, i.split('/')[0]+'/'+i.split('/')[1])
@@ -52,7 +56,7 @@ class Data:
         with open(f'{self.__table}.pkl', 'rb') as file:
             df = pickle.load(file)
         df.dropna(axis=0, how='all', inplace=True)
-        return df[df['DataMart Column'] == column_name]
+        return df[df['COLUMN.3'] == column_name]
 
         
 
